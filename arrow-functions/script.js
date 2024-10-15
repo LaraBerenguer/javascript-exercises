dad787ecb940e0b29e1ecb98e889754c4134d93e
LaraBerenguer <laraberenguer@outlook.com>
2024-10-15 19:01:15 +0200

    First commit

---
+++ arrow-functions/script.js
@@ -0,0 +1,55 @@
+//ex1
+const add = (x, y) => {
+    return x + y;
+}
+
+console.log(add(2, 3));
+
+//ex2
+const randomNumber = () => {
+    return Math.floor(Math.random() * 101);
+}
+
+console.log(randomNumber());
+
+//ex3
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const greet = () => {
+    const newPerson = new Person("Pepe");
+    console.log(`Hola, ${newPerson.name}.`);
+}
+
+greet();
+
+//ex4
+const console = (num) => {
+    console.log(num);
+}
+
+const printNumbers2 = (array) => {
+    for(i=0; i<array.length; i++) {        
+        console(array[i]);
+    }
+}
+
+const array2 = [22,5,78,12];
+printNumbers2(array2);
+
+//ex5
+
+const myFunction = () => {
+    console.log("Hola!");
+}
+setTimeout(myFunction, 3000);
+myFunction();
+
+
+
+
+
+
